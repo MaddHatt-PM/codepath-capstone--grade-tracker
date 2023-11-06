de25552f27a314cb22170a5f1b78d248b23eab34
Patt-Mac <MaddHatt.pm@gmail.com>
2023-11-05 22:59:53 -0500

    Project setup and database setup

---
+++ GradeTracker/GradeTracker/GradeTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  GradeTrackerApp.swift
+//  GradeTracker
+//
+//  Created by Patt Martin on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GradeTrackerApp: App {
+    let persistenceController = DatabaseManager.shared
+
+    var body: some Scene {
+        WindowGroup {
+            PrototypeContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
